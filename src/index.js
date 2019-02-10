0d3942a47b6f3a59db0fa9f9e43fd901e4eb1743
Nate Choi <jereno413@gmail.com>
2019-02-10 12:18:04 +0900

    Create Button

--- src/index.js
+++ src/index.js
@@ -1,49 +1,73 @@
 console.log('hello')
 
 import * as THREE from '../node_modules/three/build/three.js';
+import * as dat from 'dat.gui';
+
 
 let scene, camera, renderer, cube;
 let ADD = 0.01;
 
-let createCube = function(){
-   let geometry = new THREE.BoxGeometry(1,1,1);
-   let material = new THREE.MeshBasicMaterial({color:0x00a1cb});
-   cube = new THREE.Mesh(geometry, material)
-   scene.add(cube);
+
+let createCube = function () {
+    let geometry = new THREE.BoxGeometry(1, 1, 1);
+    let material = new THREE.MeshBasicMaterial({ color: 0x00a1cb });
+    cube = new THREE.Mesh(geometry, material)
+    scene.add(cube);
+}
+
+const params = {
+    importObj(){
+        console.log('import obj')
+    },
+    setMtl() {
+        console.log('set mtl')
+    }  ,
+    exportObj(){
+        console.log('export obj')
+    },
+}
+
+let init = function () {
+    // create the scene
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffee);
+
+    // create an locate the camera
+    camera = new THREE.PerspectiveCamera(75,
+        window.innerWidth / window.innerHeight,
+        1, 1000);
+    camera.position.z = 5;
+
+    createCube();
+
+    // create the renderer   
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+
+    const gui = new dat.GUI();
+    document.body.appendChild(renderer.domElement);
+   
+    renderer.render(scene, camera);
+
+    gui.add(params, 'importObj');
+    gui.add(params, 'setMtl');
+    gui.add(params, 'exportObj');
+    gui.open();
 };
 
-let init = function() {
-   // create the scene
-   scene = new THREE.Scene();
-   scene.background = new THREE.Color(0xffffee);
-   
-   // create an locate the camera
-   camera = new THREE.PerspectiveCamera(75, 
-               window.innerWidth / window.innerHeight, 
-               1, 1000);
-   camera.position.z = 5;
-   
-   createCube();
-   
-   // create the renderer   
-   renderer = new THREE.WebGLRenderer();   
-   renderer.setSize(window.innerWidth, window.innerHeight);
-   
-   document.body.appendChild(renderer.domElement);
- };
 
+// let mainLoop = function () {
+//     cube.position.x += ADD;
+//     cube.rotation.z -= ADD;
 
-let mainLoop = function() {
-   cube.position.x += ADD;    
-   cube.rotation.z -= ADD;
-   
-   if(cube.position.x <= -3 || cube.position.x >= 3)
-      ADD *= -1;
-   
-   renderer.render(scene, camera);
-   requestAnimationFrame(mainLoop);
- };
+//     if (cube.position.x <= -3 || cube.position.x >= 3)
+//         ADD *= -1;
+
+//     renderer.render(scene, camera);
+//     requestAnimationFrame(mainLoop);
+// };
 
 
 init()
-mainLoop()
+//mainLoop()
