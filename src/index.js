1119dce70ed211be8950402b6d5241983fe1ca4a
Nate Choi <jereno413@gmail.com>
2019-02-10 12:43:04 +0900

    Update index.js: orbitcontrol

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-console.log('hello')
-
-import * as THREE from '../node_modules/three/build/three.js';
+import * as THREE from 'three';
 import * as dat from 'dat.gui';
+import * as OrbitControls from 'three-orbitcontrols';
 
 
-let scene, camera, renderer, cube;
+let scene, camera, renderer, cube, controls;
 let ADD = 0.01;
 
 
@@ -37,37 +36,35 @@ let init = function () {
         window.innerWidth / window.innerHeight,
         1, 1000);
     camera.position.z = 5;
-
+   
     createCube();
 
     // create the renderer   
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
 
-
-    const gui = new dat.GUI();
+    controls = new OrbitControls( camera, renderer.domElement);
     document.body.appendChild(renderer.domElement);
-   
-    renderer.render(scene, camera);
-
+    
+    // renderer.render(scene, camera);
+    
+    const gui = new dat.GUI();
     gui.add(params, 'importObj');
     gui.add(params, 'setMtl');
     gui.add(params, 'exportObj');
-    gui.open();
 };
 
 
-// let mainLoop = function () {
-//     cube.position.x += ADD;
-//     cube.rotation.z -= ADD;
+let mainLoop = function () {
+    cube.position.x += ADD;
+    cube.rotation.z -= ADD;
 
-//     if (cube.position.x <= -3 || cube.position.x >= 3)
-//         ADD *= -1;
+    if (cube.position.x <= -3 || cube.position.x >= 3)
+        ADD *= -1;
 
-//     renderer.render(scene, camera);
-//     requestAnimationFrame(mainLoop);
-// };
-
+    renderer.render(scene, camera);
+    requestAnimationFrame(mainLoop);
+};
 
 init()
-//mainLoop()
+mainLoop()
