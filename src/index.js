cc7a6b84d14bcd2fea63884550a268a1ea590383
Nate Choi <jereno413@gmail.com>
2019-02-12 14:40:08 +0900

    Update resource

--- src/index.js
+++ src/index.js
@@ -29,43 +29,25 @@ var mtlLoader = new THREE.MTLLoader();
 
 const BtnParams = {
     importObj() {
-        mtlLoader.load('resource/j_test.mtl', function (materials) {
+        mtlLoader.load('resource/audi_obj.mtl', function (materials) {
 
             materials.preload();
 
             var objLoader = new THREE.OBJLoader();
             objLoader.setMaterials(materials);
             // objLoader.setPath('/examples/3d-obj-loader/assets/');
-            objLoader.load('resource/j_test.obj', function (object) {
+            objLoader.load('resource/audi_obj.obj', function (object) {
 
                 scene.add(object);
-                object.position.y -= 60;
-
+                object.position.y = -100;
             });
 
         });
-        // loader.load(
-        //     'resource/j_test.obj',
-        //     (object) => {
-        //         object.traverse(function (child) {
-        //             if (child instanceof THREE.Mesh) {
-        //                 child.material.ambient = color
-        //                 child.material.color = color
-        //             }
-        //         })
-        //         object.position.y = 0;
-        //         scene.add(object);
-        //     },
-        //     // called when loading is in progresses
-        //     (xhr) => {
-        //     },
-        //     // called when loading has errors
-        //     (error) => {
-        //     }
-        // )
     },
-    setMtl() {},
     exportObj() {},
+    setY: 2
+
+    
 }
 
 const init = function () {
@@ -76,9 +58,9 @@ const init = function () {
 
     createCube();
 
-    var geometry = new THREE.SphereGeometry(500, 60, 40);
+    var geometry = new THREE.SphereGeometry(200, 60, 40);
     // invert the geometry on the x-axis so that all of the faces point inward
-    geometry.scale(- 1, 1, 1);
+    geometry.scale( -1, 1, 1);
     var material = new THREE.MeshBasicMaterial({
         map: new THREE.TextureLoader().load('resource/background/test.jpg')
     });
@@ -101,10 +83,19 @@ const init = function () {
     scene.add( light );
     renderer.render(scene, camera);
 
-    const gui = new dat.GUI();
-    gui.add(BtnParams, 'importObj')
-    gui.add(BtnParams, 'setMtl')
-    gui.add(BtnParams, 'exportObj')
+    const panel = new dat.GUI();
+
+    const folder1 = panel.addFolder( 'Basic')
+
+    const folder2 = panel.addFolder( 'Object/Camera Controls')
+    const folder3 = panel.addFolder( 'Scene Details' );
+    const folder4 = panel.addFolder( 'VisualFX' );
+    folder1.add(BtnParams, 'importObj'),
+    folder1.add(BtnParams, 'exportObj')
+    folder1.open()
+
+    folder2.add(BtnParams,'setY', -5, 5)
+    folder2.open()
 
 }
 
@@ -112,7 +103,7 @@ const init = function () {
 const mainLoop = function () {
     cube.position.x += ADD;
     cube.rotation.z -= ADD;
-
+    cube.position.y = BtnParams.setY
     if (cube.position.x <= -3 || cube.position.x >= 3)
         ADD *= -1;
     
